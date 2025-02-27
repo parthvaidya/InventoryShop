7093bc53718795b5f19da2ab226ec4bed1b7feaf
Parth Vaidya <zeusparth99@gmail.com>
2025-02-27 18:27:10 +0530

    Coins UI added with image and text

---
+++ My project/Assets/Scripts/Shop/ShopViewPortButton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopViewPortButton : MonoBehaviour
+{
+    [System.Serializable]
+    public struct ViewportBinding
+    {
+        public Button button;      // UI Button
+        public GameObject viewport; // Corresponding viewport
+    }
+
+    public ViewportBinding[] bindings; // Array for button-viewport pairs
+
+    void Start()
+    {
+        // Assign button click events
+        for (int i = 0; i < bindings.Length; i++)
+        {
+            int index = i; // Store index to avoid closure issue
+            bindings[i].button.onClick.AddListener(() => SetActiveViewport(index));
+        }
+
+        SetActiveViewport(0); // Show "All" by default
+    }
+
+    public void SetActiveViewport(int index)
+    {
+        for (int i = 0; i < bindings.Length; i++)
+        {
+            bindings[i].viewport.SetActive(i == index);
+        }
+    }
+}
